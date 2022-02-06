f3cbdb28ed6616ef91d79e136e5f5f7cb4c4ea15
Dipanjan Bhowmik <onlydevelop@gmail.com>
2022-02-06 22:48:25 +0530

    Removed useless file

--- services/filestore/src/test/java/com/example/filestore/FileStoreApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.filestore;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FileStoreApplicationTests {
-
-}
